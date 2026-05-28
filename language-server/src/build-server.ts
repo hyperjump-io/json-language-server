3c3126fa9896f77fc3c05b2b8aaca77bc8fa4b38
Diya <diyasrivastava2023@gmail.com>
2026-05-28 15:46:19 +0530

    add JsonValidation feature class and Server service

--- language-server/src/build-server.ts
+++ language-server/src/build-server.ts
@@ -1,47 +1,21 @@
 import "@hyperjump/json-schema/draft-2020-12";
-import { TextDocuments, TextDocumentSyncKind, Diagnostic, DiagnosticSeverity } from "vscode-languageserver";
+import { TextDocuments } from "vscode-languageserver";
 import { TextDocument } from "vscode-languageserver-textdocument";
-import * as jsonc from "jsonc-parser";
+import { Server } from "./services/server.ts";
+import { JsonValidation } from "./features/JsonValidation.ts";
 
-import type { ServerCapabilities, Connection } from "vscode-languageserver";
+import type { Connection } from "vscode-languageserver";
 
 export type LanguageServerSettings = {
 };
 
-export const buildServer = (server: Connection): Connection => {
+export const buildServer = (connection: Connection): Connection => {
+  const server = new Server(connection);
+
   const documents = new TextDocuments(TextDocument);
   documents.listen(server);
 
-  server.onInitialize(() => {
-    const serverCapabilities: ServerCapabilities = {
-      textDocumentSync: TextDocumentSyncKind.Incremental
-    };
-
-    return {
-      capabilities: serverCapabilities
-    };
-  });
-
-  documents.onDidChangeContent(async (change) => {
-    const textDocument = change.document;
-    const text = textDocument.getText();
-    const parseErrors: jsonc.ParseError[] = [];
-
-    jsonc.parseTree(text, parseErrors);
-
-    // for syntax error
-    const syntaxDiagnostics: Diagnostic[] = parseErrors.map((error) => ({
-      severity: DiagnosticSeverity.Error,
-      range: {
-        start: textDocument.positionAt(error.offset),
-        end: textDocument.positionAt(error.offset + error.length)
-      },
-      message: jsonc.printParseErrorCode(error.error),
-      source: "json-language-server"
-    }));
-
-    void server.sendDiagnostics({ uri: textDocument.uri, diagnostics: [...syntaxDiagnostics] });
-  });
+  new JsonValidation(server, documents);
 
   return server;
 };
