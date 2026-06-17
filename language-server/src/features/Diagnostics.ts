cf2e482bafffef6c6c235ba41d5e2184e9cbb9e0
Diya <diyasrivastava2023@gmail.com>
2026-06-18 02:42:46 +0530

    align Diagnostics with upstream: use JsonDocuments type, remove clearCache

--- language-server/src/features/Diagnostics.ts
+++ language-server/src/features/Diagnostics.ts
@@ -1,24 +1,20 @@
 import { Server } from "../services/server.ts";
+import { JsonDocuments } from "../services/JsonDocuments.ts";
 import { JsonDocument } from "../models/JsonDocument.ts";
 
-import type { Diagnostic, TextDocuments } from "vscode-languageserver";
+import type { Diagnostic } from "vscode-languageserver";
 
 export type DiagnosticsProvider = {
   getDiagnostics(jsonDocument: JsonDocument): Promise<Diagnostic[]>;
-  clearCache?(document: JsonDocument): void;
 };
 
 export class Diagnostics {
   private providers: DiagnosticsProvider[];
 
-  constructor(server: Server, documents: TextDocuments<JsonDocument>, providers: DiagnosticsProvider[]) {
+  constructor(server: Server, documents: JsonDocuments, providers: DiagnosticsProvider[]) {
     this.providers = providers;
 
     documents.onDidChangeContent(async (change) => {
-      for (const provider of this.providers) {
-        provider.clearCache?.(change.document);
-      }
-
       const diagnostics = [];
       for (const provider of this.providers) {
         diagnostics.push(...await provider.getDiagnostics(change.document));
